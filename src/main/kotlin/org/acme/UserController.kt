8cb6ede8c473000ba1d85bfa4815f850f4cb9cea
Ivan Bazalii <bazalijivan2002@yandex.ru>
2023-01-25 01:01:55 +0300

    feat: realise UserController

---
+++ src/main/kotlin/org/acme/UserController.kt
@@ -0,0 +1,54 @@
+package org.acme
+
+import io.smallrye.graphql.api.Context
+import io.smallrye.graphql.api.Subscription
+import io.smallrye.mutiny.Multi
+import io.smallrye.mutiny.operators.multi.processors.BroadcastProcessor
+import org.eclipse.microprofile.graphql.*
+import java.util.*
+import javax.enterprise.context.RequestScoped
+import kotlin.collections.ArrayList
+
+@GraphQLApi
+@RequestScoped
+class UserController {
+    private var _users = ArrayList<User>()
+
+    companion object {
+        var BroadcastProcessor: BroadcastProcessor<User> =
+            io.smallrye.mutiny.operators.multi.processors.BroadcastProcessor.create()
+    }
+
+    @Query
+    fun getUser(id: UUID): User {
+        return _users.first { user -> user.id == id }
+    }
+
+    @Mutation("addUser")
+    fun addUser(user: User): User {
+        _users.add(user)
+        BroadcastProcessor.onNext(user)
+
+        return user
+    }
+
+    @Subscription
+    fun subscribeToUsersWithFilter(context: Context, token: String, userName: String): Multi<User>? {
+        isValid(token)
+
+        return BroadcastProcessor.filter { user -> user!!.name == userName && isValid(token) }
+    }
+
+    @Subscription
+    fun subscribeToUsers(): Multi<User> {
+        return BroadcastProcessor
+    }
+
+    private fun isValid(token: String): Boolean {
+        if (token != "123") {
+            throw TokenValidationException("Invalid token!")
+        }
+
+        return true
+    }
+}
